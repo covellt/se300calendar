817841ea9bd49b02581cdd658ff25529fda50a1f
Tyler Covell <47903355+covellt@users.noreply.github.com>
2023-10-02 21:03:10 -0400

    Develop (#2)
    * Initialize Firebase
    
    Initialized Firebase access key and set up the work environment for our first commit.
    
    * Remove Firebase Analytics
    
    Firebase analytics does not work with node.js
    
    * Create package-lock.json

---
+++ test.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDXpg8capdPLMxvhOTIQe5Gk_4MoLM0Auw",
+  authDomain: "se300-calendar.firebaseapp.com",
+  projectId: "se300-calendar",
+  storageBucket: "se300-calendar.appspot.com",
+  messagingSenderId: "495446267648",
+  appId: "1:495446267648:web:fcdc21cf2ae117c068c5e5",
+  measurementId: "G-44RE8CBD65"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+console.log("Hello World!")
